3793ffda2abcc4bc17d7b6ba4967ded4c2279865
Jesus-Eder-Gonzalez <jesus.eder.gonzalez@gmail.com>
2018-08-20 15:57:29 -1000

    Changed the readme to the project readme.

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  console.log('smoketest');
+  res.json({ message: 'smoke test' });
+});
+
+app.listen(PORT, 'localhost', () => {
+  console.log(`SERVER LISTENING ON PORT ${PORT}`);
+});
